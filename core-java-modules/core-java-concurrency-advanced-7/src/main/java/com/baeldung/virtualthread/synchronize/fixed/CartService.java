e2d958e91387c796c4b8f12eb6217f96754613b8
saikatcse03 <saikatcse03@gmail.com>
2026-03-31 13:05:12 +0530

    Implement virtual thread pinning examples

---
+++ core-java-modules/core-java-concurrency-advanced-7/src/main/java/com/baeldung/virtualthread/synchronize/fixed/CartService.java
@@ -0,0 +1,51 @@
+package com.baeldung.virtualthread.synchronize.fixed;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CartService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CartService.class);
+
+    private final Map<String, Integer> products;
+    private final Map<String, ReentrantLock> locks = new ConcurrentHashMap<>();
+
+    public CartService() {
+        this.products = new HashMap<>();
+    }
+
+    public void update(String productId, int quantity) {
+        Lock lock = locks.computeIfAbsent(productId, k -> new ReentrantLock());
+
+        try {
+            if (lock.tryLock(500, TimeUnit.MILLISECONDS)) {
+                simulateAPI();
+                products.merge(productId, quantity, Integer::sum);
+
+                LOGGER.info("Updated Cart for {} {}", productId, quantity);
+                lock.unlock();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void simulateAPI() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<String, Integer> getProducts() {
+        return Map.copyOf(products);
+    }
+}
